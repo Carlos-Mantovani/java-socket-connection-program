f2e7da8ddadbbebd106fa33b2ea6d73cfb767dbc
carlos <sherifinho.augusto@gmail.com>
2023-06-27 21:17:55 -0300

    Creating the server and client applications

--- src/Server.java
+++ src/Server.java
@@ -5,21 +5,18 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    private Socket socket = null;
-    private ServerSocket server = null;
-    private DataInputStream in = null;
 
     public Server(int port) {
         try {
-            server = new ServerSocket(port);
+            ServerSocket server = new ServerSocket(port);
             System.out.println("Server started");
 
             System.out.println("Waiting for a client ...");
 
-            socket = server.accept();
+            Socket socket = server.accept();
             System.out.println("Client accepted");
 
-            in = new DataInputStream(
+            DataInputStream in = new DataInputStream(
                     new BufferedInputStream(socket.getInputStream()));
 
             String line = "";
