060911322a59371f3add0141cf14ec3a62825683
carlos <sherifinho.augusto@gmail.com>
2023-06-27 21:19:51 -0300

    Some changes

--- src/Client.java
+++ src/Client.java
@@ -46,7 +46,7 @@ public class Client {
         }
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Client client = new Client("127.0.0.1", 5000);
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -39,7 +39,7 @@ public class Server {
         }
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Server server = new Server(5000);
     }
 }
